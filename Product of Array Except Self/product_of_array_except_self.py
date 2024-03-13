6a6814f5f3bd0f66a2c36edb4fa6c6600c7bfc5d
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-12 22:24:16 -0500

    Product of Array Except Self

---
+++ Product of Array Except Self/product_of_array_except_self.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        right = [1] * len(nums)
+        prod = 1
+
+        # iterate in backwards order
+        for i in range(len(nums) -2, -1, -1):
+            prod *= nums[i+1]
+            right[i] = prod
+
+        prod = 1
+        for i in range(1, len(nums)):
+            prod *= nums[i-1]
+            right[i] *= prod
+
+        return right
+
+# driver function
+print(Solution().productExceptSelf([1, 2, 3, 4]))
+# [24, 12, 8, 6]
