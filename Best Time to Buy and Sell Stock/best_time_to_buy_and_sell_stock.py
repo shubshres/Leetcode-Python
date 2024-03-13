f821fb0917be0ed6574305e0f686f21db60da014
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-12 21:25:26 -0500

    Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,21 @@
+# def buy_and_sell_brute_force(arr):
+#     # brute force
+#     max_profit = 0
+
+#     for i in range(len(arr)):
+#         for j in range(i, len(arr)):
+#             max_profit = max(max_profit, arr[j]-arr[i])
+#     return max_profit
+
+def buy_and_sell(arr):
+    max_current_price = 0
+    max_profit = 0
+
+    for price in arr[::-1]:
+        max_current_price = max(max_current_price, price)
+        max_profit = max(max_profit, max_current_price-price)
+
+    return max_profit
+
+# Driver function
+print(buy_and_sell([9, 11, 8, 5, 7, 10]))
