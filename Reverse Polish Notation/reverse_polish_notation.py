00abff2136ad27e1a5ea976333737ce97d1d86f1
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-27 22:56:20 -0500

    rpn

---
+++ Reverse Polish Notation/reverse_polish_notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens):
+        stack = []
+        for c in tokens:
+            if c == "+":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b + a)
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b * a)
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+
+        return stack[0]
